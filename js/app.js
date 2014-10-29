a0003991d48d88a00ce00958181b6879055acc0c
Bill <billsearle@me.com>
2014-10-29 21:42:11 +1100

    search, add form and routes

--- js/app.js
+++ js/app.js
@@ -1,103 +1,3 @@
-/**
- * Checklist-model
- * AngularJS directive for list of checkboxes
- */
-
-angular.module('checklist-model', [])
-.directive('checklistModel', ['$parse', '$compile', function($parse, $compile) {
-  // contains
-  function contains(arr, item) {
-    if (angular.isArray(arr)) {
-      for (var i = 0; i < arr.length; i++) {
-        if (angular.equals(arr[i], item)) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-  // add
-  function add(arr, item) {
-    arr = angular.isArray(arr) ? arr : [];
-    for (var i = 0; i < arr.length; i++) {
-      if (angular.equals(arr[i], item)) {
-        return arr;
-      }
-    }    
-    arr.push(item);
-    return arr;
-  }  
-
-  // remove
-  function remove(arr, item) {
-    if (angular.isArray(arr)) {
-      for (var i = 0; i < arr.length; i++) {
-        if (angular.equals(arr[i], item)) {
-          arr.splice(i, 1);
-          break;
-        }
-      }
-    }
-    return arr;
-  }
-
-  // http://stackoverflow.com/a/19228302/1458162
-  function postLinkFn(scope, elem, attrs) {
-    // compile with `ng-model` pointing to `checked`
-    $compile(elem)(scope);
-
-    // getter / setter for original model
-    var getter = $parse(attrs.checklistModel);
-    var setter = getter.assign;
-
-    // value added to list
-    var value = $parse(attrs.checklistValue)(scope.$parent);
-
-    // watch UI checked change
-    scope.$watch('checked', function(newValue, oldValue) {
-      if (newValue === oldValue) { 
-        return;
-      } 
-      var current = getter(scope.$parent);
-      if (newValue === true) {
-        setter(scope.$parent, add(current, value));
-      } else {
-        setter(scope.$parent, remove(current, value));
-      }
-    });
-
-    // watch original model change
-    scope.$parent.$watch(attrs.checklistModel, function(newArr, oldArr) {
-      scope.checked = contains(newArr, value);
-    }, true);
-  }
-
-  return {
-    restrict: 'A',
-    priority: 1000,
-    terminal: true,
-    scope: true,
-    compile: function(tElement, tAttrs) {
-      if (tElement[0].tagName !== 'INPUT' || !tElement.attr('type', 'checkbox')) {
-        throw 'checklist-model should be applied to `input[type="checkbox"]`.';
-      }
-
-      if (!tAttrs.checklistValue) {
-        throw 'You should provide `checklist-value`.';
-      }
-
-      // exclude recursion
-      tElement.removeAttr('checklist-model');
-      
-      // local scope var storing individual checkbox model
-      tElement.attr('ng-model', 'checked');
-
-      return postLinkFn;
-    }
-  };
-}]);
-
 console.log("app.js");
 var app = angular.module("theMusoList", ["firebase", "ngRoute", "checklist-model"])
 
@@ -106,17 +6,33 @@ var app = angular.module("theMusoList", ["firebase", "ngRoute", "checklist-model
 .factory('Musos', function($firebase, fbURL) {
     return $firebase(new Firebase(fbURL)).$asArray();
 })
-//  // ROUTES
-//  .config(function($routeProvider) {
-//   $routeProvider
-//   .when('/', {
-//     controller:'HomeCtrl',
-//     templateUrl:'home.html'
-// })
-//   .otherwise({
-//     redirectTo:'/'
-// });
-// })
+
+ // ROUTES
+    .config(function($routeProvider) {
+        $routeProvider
+        .when('/', {
+            controller:'MusoCtrl',
+            templateUrl:'search.html'
+        })
+        .when('/add-muso', {
+            controller:'MusoCtrl',
+            templateUrl:'add-muso.html'
+        })
+        .when('/new', {
+            controller:'TacoCtrl',
+            templateUrl:'order.html'
+        })
+        .when('/confirmation', {
+            controller:'ConfCtrl',
+            templateUrl:'confirmation.html'
+        })
+        .when('/soldout',{
+            templateUrl:'sold-out.html'
+        })
+        .otherwise({
+            redirectTo:'/'
+        });
+    })
 
 .controller('MusoCtrl', function($scope, Musos) {
     // sets $scope to Muso fatory of firebase
@@ -130,8 +46,8 @@ var app = angular.module("theMusoList", ["firebase", "ngRoute", "checklist-model
         "bass",
         "drums",
         "double bass",
-        "a guitar",
-        "sax",
+        "acoustic guitar",
+        "saxaphone",
         "trumpet",
         "trombone",
         "violin",
@@ -144,7 +60,7 @@ var app = angular.module("theMusoList", ["firebase", "ngRoute", "checklist-model
         "writer",
         "agent",
         "photographer",
-        "camera man / videographer"
+        "camera man"
     ];
 
     $scope.addMuso = function() {
